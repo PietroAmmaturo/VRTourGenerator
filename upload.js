f148f49c7c9a3978c6aad9f7441afdf584154134
pietro <pammaturo02@gmail.com>
2023-09-02 16:39:18 +0200

    support for file uploads by click

--- upload.js
+++ upload.js
@@ -1,8 +1,9 @@
 class UploadManager {
-    constructor(dropzoneSelector, containerSelector) {
+    constructor(dropzoneSelector, containerSelector, fileInputSelector) {
         this.dropzone = document.querySelector(dropzoneSelector);
         this.thumbnailsContainer = document.querySelector(containerSelector);
         this.reorderManager = new ReorderManager(".thumbnail");
+        this.fileInput = document.querySelector(fileInputSelector);
 
         this.dropzone.addEventListener("dragover", (e) => {
             e.preventDefault();
@@ -44,7 +45,36 @@ class UploadManager {
                 }
             }
         });
+
+        this.fileInput.addEventListener("change", (e) => {
+            const files = e.target.files;
+            for (const file of files) {
+                if (file.type.startsWith("image/")) {
+                    const reader = new FileReader();
+                    reader.onload = (event) => {
+                        const img = new Image();
+                        img.onload = () => {
+                            const thumbnailSize = 150; // Adjust this value as needed
+                            const aspectRatio = img.width / img.height;
+                            img.width = thumbnailSize;
+                            img.height = thumbnailSize / aspectRatio;
+                        };
+                        img.src = event.target.result;
+                        img.className = "thumbnail";
+                        img.draggable = true;
+                        img.addEventListener("dragstart", (e) => {
+                            e.dataTransfer.setData("text/plain", img.src);
+                        });
+                        const wrapper = document.createElement("div");
+                        wrapper.appendChild(img);
+                        this.thumbnailsContainer.appendChild(wrapper);
+                        this.reorderManager.refresh();
+                    };
+                    reader.readAsDataURL(file);
+                }
+            }
+        });
     }
 }
 
-const uploadManager = new UploadManager("#dropzone", "#container");
+const uploadManager = new UploadManager("#dropzone", "#container", "#fileInput");
